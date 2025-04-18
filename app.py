ab3b45529862893f1e58430bfceebed7223ced10
Lalitaditya Charapanjeri <122503975+lalitaditya16@users.noreply.github.com>
2025-04-18 18:40:16 -0400

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ from dotenv import load_dotenv
 
 
 # Retrieve API URL and API Key from environment variables
-API_URL = "https://api-inference.huggingface.co/models/mistralai/Mistral-7B-Instruct-v0.1"
+API_URL = "https://api-inference.huggingface.co/models/tiiuae/falcon-7b-instruct"
 API_KEY = os.getenv("HUGGINGFACE_API_KEY")
 
 # Check if the API URL and API Key are available
